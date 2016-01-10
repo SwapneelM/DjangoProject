0810b0a8576d1465f65957c86ccde762c24a13af
SwapneelM <swapneel.mehta@gmail.com>
2016-01-10 18:10:23 +0530

    first commit

---
+++ xmas1/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Xmas1Config(AppConfig):
+    name = 'xmas1'
---
+++ xmas2/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Xmas2Config(AppConfig):
+    name = 'xmas2'
